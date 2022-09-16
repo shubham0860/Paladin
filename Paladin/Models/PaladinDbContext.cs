5f0be7cfc50e949d0b757aaa5c66355afa2c6c36
S906717 <S906717@S906717-W10>
2022-09-16 23:18:45 +0530

    Add project files.

---
+++ Paladin/Models/PaladinDbContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Paladin.Models
+{
+    public class PaladinDbContext : DbContext
+    {
+        public PaladinDbContext()
+            : base("name=Paladin")
+        {
+            Database.SetInitializer<PaladinDbContext>(new DropCreateDatabaseIfModelChanges<PaladinDbContext>());
+        }
+
+        public DbSet<Applicant> Applicants { get; set; }
+        public DbSet<Employment> Employment { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<Products> Products { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
+
+    public class PaladinInitializer : DropCreateDatabaseIfModelChanges<PaladinDbContext>
+    {
+        protected override void Seed(PaladinDbContext context)
+        {
+            base.Seed(context);
+        }
+    }
+}
